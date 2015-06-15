0356bd8cfd0ea96a1452d6f6c682f19be3bc9cbc
Sam Kerr <kerr.sam@gmail.com>
2015-06-14 21:08:08 -0400

    Convert the add function to 'impl' for BigInt structs.

--- src/main.rs
+++ src/main.rs
@@ -22,72 +22,81 @@ fn add_with_carry(a: u64, b:u64) -> (u64, u64) {
     };
 }
 
-fn add(a: &BigInt, b: &BigInt) -> BigInt {
-    let mut result:BigInt = BigInt {length:0, data: vec![] };
-
-    // Add each of the u64 for a&b until there aren't anymore
-    let mut carry:u64 = 0;
-    for i in 0..std::cmp::min(a.length, b.length) {
-
-        // Add the raw values
-        let (interim, internal_carry) = add_with_carry(a.data[i],b.data[i]);
-        let temp_carry:u64= internal_carry + carry;
-
-        // Add the previous carry value
-        let (interim, internal_carry) = add_with_carry(interim, carry);
-        carry = internal_carry + temp_carry;
-
-        // Add the digit to the BigInt
-        result.data.push(interim);
-        result.length = result.length + 1;
-    }
-
-    // Add in the longer of the two values
-    let (longer, starting_index) = match a.length == b.length {
-        true => (None, 0),
-        false => match  a.length > b.length {
-            true => (Some(a), a.length - b.length),
-            false => (Some(b), b.length - a.length)
-        }
-    };
-
-    // Find the longer integer if it is there
-    match longer {
-        Some(x) => {
-            println!("Unequal sizes, parsing the longer.");
-            for i in starting_index..x.length {
-                let (next, next_carry) = add_with_carry(x.data[i], carry);
-                carry = next_carry;
-                result.data.push(next);
-                result.length = result.length + 1;
-            }
-        },
-        None => println!("Same length.")
-    }
-     
-    // Add the final carry if there is one
-    if carry > 0 {
-        result.data.push(carry);
-        result.length = result.length + 1;
-    }
+impl BigInt {
     
-    return result;
+    fn add(&self, b: &BigInt) -> BigInt {
+        let mut result:BigInt = BigInt {length:0, data: vec![] };
+
+        // Add each of the u64 for a&b until there aren't anymore
+        let mut carry:u64 = 0;
+        for i in 0..std::cmp::min(self.length, b.length) {
+
+            // Add the raw values
+            let (interim, internal_carry) = add_with_carry(self.data[i], b.data[i]);
+            let temp_carry:u64= internal_carry + carry;
+
+            // Add the previous carry value
+            let (interim, internal_carry) = add_with_carry(interim, carry);
+            carry = internal_carry + temp_carry;
+
+            // Add the digit to the BigInt
+            result.data.push(interim);
+            result.length = result.length + 1;
+        }
+
+        // Add in the longer of the two values
+        let (longer, starting_index) = match self.length == b.length {
+            true => (None, 0),
+            false => match  self.length > b.length {
+                true => (Some(self), self.length - b.length),
+                false => (Some(b), b.length - self.length)
+            }
+        };
+
+        // Find the longer integer if it is there
+        match longer {
+            Some(x) => {
+                println!("Unequal sizes, parsing the longer.");
+                for i in starting_index..x.length {
+                    let (next, next_carry) = add_with_carry(x.data[i], carry);
+                    carry = next_carry;
+                    result.data.push(next);
+                    result.length = result.length + 1;
+                }
+            },
+            None => println!("Same length.")
+        }
+         
+        // Add the final carry if there is one
+        if carry > 0 {
+            result.data.push(carry);
+            result.length = result.length + 1;
+        }
+        
+        return result;
+    }
 }
 
 fn main() {
     println!("Hello, world!");
 
-    // Create some integers
+    // Create some integers. Make them overflow.
     let a = create_bigint_from_string("FFFFFFFFFFFFFFFF");
     let b = create_bigint_from_string("FFFFFFFFFFFFFFFF");
-    let c = add(&a, &b);
-    let d = add(&c, &b);
+    let c = a.add(&b);
+    let d = c.add(&b);
 
     // Print the results
     print_bigint(&a);
     print_bigint(&b);
     print_bigint(&c);
     print_bigint(&d);
+
+    // Do 5+10=15
+    let five = create_bigint_from_string("5");
+    let ten = create_bigint_from_string("10");
+    let fifteen = five.add(&ten);
+    print_bigint(&fifteen);
 }
 
 fn print_bigint(val: &BigInt) {
@@ -150,8 +159,4 @@ fn create_bigint_from_string(val: &str) -> BigInt {
     }
 
     return BigInt { length:the_data.len(), data: the_data} ;
-}
-
-fn create_bigint(val:u64) -> BigInt {
-    return BigInt { length:1, data:vec![val] } ;
 }
