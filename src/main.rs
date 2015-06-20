b671a06c327571e17f81d40fce96dfd7a88e1e6f
Sam Kerr <kerr.sam@gmail.com>
2015-06-20 11:54:46 -0400

    Adding some subtraction tests (that pass! yay!)

--- src/main.rs
+++ src/main.rs
@@ -707,4 +707,121 @@ mod tests {
         assert!(c.negative == true);
         assert!(c.data[0] == 6);
     }
+
+    #[test]
+    fn test_basic_sub_positive_positive_nocarry_positiveresult() {
+        let mut a_result:Result<BigInt,Error> = create_bigint_from_string("7");
+        let a:BigInt = match a_result {
+            Ok(v) => v,
+            Err(e) => panic!(e)
+        };
+
+        let mut b_result:Result<BigInt,Error> = create_bigint_from_string("1");
+        let b:BigInt = match b_result {
+            Ok(v) => v,
+            Err(e) => panic!(e)
+        };
+        let c:BigInt = a.subtract(&b);
+        assert!(c.length == 1);
+        assert!(c.negative == false);
+        assert!(c.data[0] == 6);
+    }
+
+    #[test]
+    fn test_basic_sub_positive_positive_nocarry_negativeresult() {
+        let mut a_result:Result<BigInt,Error> = create_bigint_from_string("7");
+        let a:BigInt = match a_result {
+            Ok(v) => v,
+            Err(e) => panic!(e)
+        };
+
+        let mut b_result:Result<BigInt,Error> = create_bigint_from_string("8");
+        let b:BigInt = match b_result {
+            Ok(v) => v,
+            Err(e) => panic!(e)
+        };
+        let c:BigInt = a.subtract(&b);
+        assert!(c.length == 1);
+        assert!(c.negative == true);
+        assert!(c.data[0] == 1);
+    }
+
+    #[test]
+    fn test_basic_sub_positive_negative_nocarry_positiveresult() {
+        let mut a_result:Result<BigInt,Error> = create_bigint_from_string("7");
+        let a:BigInt = match a_result {
+            Ok(v) => v,
+            Err(e) => panic!(e)
+        };
+
+        let mut b_result:Result<BigInt,Error> = create_bigint_from_string("-3");
+        let b:BigInt = match b_result {
+            Ok(v) => v,
+            Err(e) => panic!(e)
+        };
+
+        let c:BigInt = a.subtract(&b);
+        assert!(c.length == 1);
+        assert!(c.negative == false);
+        assert!(c.data[0] == 10);
+    }
+
+    #[test]
+    fn test_basic_sub_negative_positive_nocarry() {
+        let mut a_result:Result<BigInt,Error> = create_bigint_from_string("-7");
+        let a:BigInt = match a_result {
+            Ok(v) => v,
+            Err(e) => panic!(e)
+        };
+
+        let mut b_result:Result<BigInt,Error> = create_bigint_from_string("8");
+        let b:BigInt = match b_result {
+            Ok(v) => v,
+            Err(e) => panic!(e)
+        };
+        let c:BigInt = a.subtract(&b);
+        assert!(c.length == 1);
+        assert!(c.negative == true);
+        assert!(c.data[0] == 15);
+    }    
+
+    #[test]
+    fn test_basic_sub_negative_negative_nocarry_positiveresult() {
+        let mut a_result:Result<BigInt,Error> = create_bigint_from_string("-3");
+        let a:BigInt = match a_result {
+            Ok(v) => v,
+            Err(e) => panic!(e)
+        };
+
+        let mut b_result:Result<BigInt,Error> = create_bigint_from_string("-4");
+        let b:BigInt = match b_result {
+            Ok(v) => v,
+            Err(e) => panic!(e)
+        };
+        let c:BigInt = a.subtract(&b);
+        assert!(c.length == 1);
+        assert!(c.negative == false);
+        assert!(c.data[0] == 1);
+    }
+
+    #[test]
+    fn test_basic_sub_negative_negative_nocarry_negativeresult() {
+        let mut a_result:Result<BigInt,Error> = create_bigint_from_string("-3");
+        let a:BigInt = match a_result {
+            Ok(v) => v,
+            Err(e) => panic!(e)
+        };
+
+        let mut b_result:Result<BigInt,Error> = create_bigint_from_string("-2");
+        let b:BigInt = match b_result {
+            Ok(v) => v,
+            Err(e) => panic!(e)
+        };
+        let c:BigInt = a.subtract(&b);
+        assert!(c.length == 1);
+        assert!(c.negative == true);
+        assert!(c.data[0] == 1);
+    }
+
+
 }
