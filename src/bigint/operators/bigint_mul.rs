722f0775a3dcb13e1b784a975e8b2f661022f6cd
Sam Kerr <skerr@gitlab.com>
2021-06-04 14:27:08 -0400

    Multiply is overflowing

--- src/bigint/operators/bigint_mul.rs
+++ src/bigint/operators/bigint_mul.rs
@@ -5,11 +5,26 @@ use std::ops::*;
 use std::cmp::*;
 
 
-fn karatsuba_mul(a: &BigInt, b: &BigInt) -> BigInt {
+fn karatsuba_mul(a_orig: &BigInt, b_orig: &BigInt) -> BigInt {
+
+    let stored_negative;
+    if (a_orig.negative && !b_orig.negative) || (!a_orig.negative && b_orig.negative) {
+        stored_negative = true;
+    }
+    else {
+        stored_negative = false;
+    }
+    
+    let mut a:BigInt = a_orig.clone();
+    let mut b:BigInt = b_orig.clone();
+    a.negative = false;
+    b.negative = false;
+
+
     // From https://en.wikipedia.org/wiki/Karatsuba_algorithm#Pseudocode
     //let ten:BigInt  = create_bigint_from_string("0x10").unwrap();
     let two:BigInt  = create_bigint_from_string("0x2").unwrap();
-    if a < &two || b < &two {
+    if a < two || b < two {
         return a * b;
     }
 
@@ -56,7 +71,11 @@ fn karatsuba_mul(a: &BigInt, b: &BigInt) -> BigInt {
 
     let mut second:BigInt = two.clone() << &m2_bi;
     second = second * (z1 - z2 - &z0);
-    return first + second + &z0;
+    let mut result:BigInt = first + second + &z0;
+
+    result.negative = stored_negative;
+
+    return result;
 }
 
 
@@ -87,42 +106,6 @@ impl<'a,'b> Mul<&'a BigInt> for &'b BigInt {
     type Output = BigInt;
 
     fn mul(self, b: &'a BigInt) -> BigInt {
-        /*
-         * From Wikipedia
-        c = 0
-
-        while b ≠ 0
-            if (b and 1) ≠ 0
-                c = c + a
-            left shift a by 1
-            right shift b by 1
-
-        return c
-        */
-        let zero:BigInt  = create_bigint_from_string("0x0").unwrap();
-        
-        if self.compare(&zero) == 0 || b.compare(&zero) == 0 {
-            // Hardcode multiply by zero
-            return zero.clone();
-        }
-
-        let one:BigInt = create_bigint_from_string("0x1").unwrap();
-        let mut c:BigInt = zero.clone();
-        let mut a_copy:BigInt = self.clone();
-        let mut b_copy:BigInt = b.to_owned();
-        while (b_copy.compare(&zero)) != 0 {
-            if (&b_copy & &one).compare(&zero) != 0 {
-                c = &c + &a_copy;
-            }
-            a_copy = &a_copy << &one;
-            b_copy = &b_copy >> &one;
-        }
-        if (self.negative && !b.negative) || (!self.negative && b.negative) {
-            c.negative = true;
-        } else {
-            c.negative = false;
-        }
-
-        return c;
+        return karatsuba_mul(&self, &b);
     }
 }
