00953966d072e9db15df52cd7cfb9529816fddba
Sam Kerr <skerr@gitlab.com>
2021-06-04 14:20:40 -0400

    Initial build of karatsuba multiplication

--- src/bigint/operators/bigint_mul.rs
+++ src/bigint/operators/bigint_mul.rs
@@ -2,16 +2,63 @@ use ::bigint::BigInt;
 use ::bigint::utilities::*;
 
 use std::ops::*;
+use std::cmp::*;
 
-/*
+
 fn karatsuba_mul(a: &BigInt, b: &BigInt) -> BigInt {
-    if (a < 10 || b < 10) {
+    // From https://en.wikipedia.org/wiki/Karatsuba_algorithm#Pseudocode
+    //let ten:BigInt  = create_bigint_from_string("0x10").unwrap();
+    let two:BigInt  = create_bigint_from_string("0x2").unwrap();
+    if a < &two || b < &two {
         return a * b;
     }
-    let zero:BigInt  = create_bigint_from_string("0x0").unwrap();
-    return zero;
+
+    /* Calculates the size of the numbers. */
+    //m = min(size_base10(num1), size_base10(num2))
+    let m:usize = min(a.data.len(), b.data.len());
+    let m2:usize = m / 2; 
+    /* m2 = ceil(m / 2) will also work */
+
+    //let m_bi:BigInt = BigInt { negative:false, data: vec![m as u64]};
+    let m2_bi:BigInt = BigInt { negative:false, data: vec![m2 as u64]};
+
+    /* Split the digit sequences in the middle. */
+    let high1:BigInt = BigInt {
+        negative: a.negative,
+        data: a.data.get(0..m2).unwrap().to_vec()
+    };
+    let low1:BigInt = BigInt {
+        negative: a.negative,
+        data: a.data.get(m2..a.data.len()).unwrap().to_vec()
+    };
+
+    let high2:BigInt = BigInt {
+        negative: b.negative,
+        data: b.data.get(0..m2).unwrap().to_vec()
+    };
+    let low2:BigInt = BigInt {
+        negative: b.negative,
+        data: b.data.get(m2..b.data.len()).unwrap().to_vec()
+    };
+
+    /*
+    high1, low1 = split_at(num1, m2)
+    high2, low2 = split_at(num2, m2)
+    */
+
+    /* 3 calls made to numbers approximately half the size. */
+    let z0:BigInt = karatsuba_mul(&low1, &low2);
+    let z1:BigInt = karatsuba_mul(&(low1 + high1.clone()), &(low2 + high2.clone()));
+    let z2:BigInt = karatsuba_mul(&high1, &high2);
+
+    let mut first:BigInt = &two << (&m2_bi * &two);
+    first = first * &z2;
+
+    let mut second:BigInt = two.clone() << &m2_bi;
+    second = second * (z1 - z2 - &z0);
+    return first + second + &z0;
 }
-*/
+
 
 impl Mul for BigInt {
     type Output = BigInt;
