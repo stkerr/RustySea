33748af9a0bbfdb9f71486b7760e2351272b1cb0
Sam Kerr <skerr@gitlab.com>
2021-06-14 16:16:11 -0400

    Re-add gradeschool multiply for small multiply

--- src/bigint/operators/bigint_mul.rs
+++ src/bigint/operators/bigint_mul.rs
@@ -4,6 +4,48 @@ use ::bigint::utilities::*;
 use std::ops::*;
 use std::cmp::*;
 
+
+fn gradeschool_mul(a: &BigInt, b: &BigInt) -> BigInt {
+        /*
+         * From Wikipedia
+        c = 0
+
+        while b ≠ 0
+            if (b and 1) ≠ 0
+                c = c + a
+            left shift a by 1
+            right shift b by 1
+
+        return c
+        */
+        let zero:BigInt  = create_bigint_from_string("0x0").unwrap();
+
+        if a.compare(&zero) == 0 || b.compare(&zero) == 0 {
+            // Hardcode multiply by zero
+            return zero.clone();
+        }
+
+        let one:BigInt = create_bigint_from_string("0x1").unwrap();
+        let mut c:BigInt = zero.clone();
+        let mut a_copy:BigInt = a.clone();
+        let mut b_copy:BigInt = b.to_owned();
+        while (b_copy.compare(&zero)) != 0 {
+            if (&b_copy & &one).compare(&zero) != 0 {
+                c = &c + &a_copy;
+            }
+            a_copy = &a_copy << &one;
+            b_copy = &b_copy >> &one;
+        }
+        if (a.negative && !b.negative) || (!a.negative && b.negative) {
+            c.negative = true;
+        } else {
+            c.negative = false;
+        }
+
+        return c;
+    }
+
+
 fn karatsuba_mul(a_orig: &BigInt, b_orig: &BigInt) -> BigInt {
 
     let stored_negative;
@@ -24,14 +66,17 @@ fn karatsuba_mul(a_orig: &BigInt, b_orig: &BigInt) -> BigInt {
     //let ten:BigInt  = create_bigint_from_string("0x10").unwrap();
     let two:BigInt = create_bigint_from_string("0x2").unwrap();
 
-println!("{} x {}\n",a,b);
-    if a < two || b < two {
-        return BigInt {negative:stored_negative, data: vec![a.data[0] * b.data[0]]};
+    let max_size:BigInt = create_bigint_from_string("0xffffffffffffffff").unwrap();
+
+println!("mult: {} x {}\n",a,b);
+println!("max:  {}\n", max_size);
+    if a <= max_size && b <= max_size {
+        return gradeschool_mul(&a_orig, &b_orig);
     }
 
     /* Calculates the size of the numbers. */
     //m = min(size_base10(num1), size_base10(num2))
-    let m:usize = min(a.data.len(), b.data.len());
+    let m:usize = max(a.data.len(), b.data.len());
     let m2:usize = m / 2; 
     /* m2 = ceil(m / 2) will also work */
     //let m_bi:BigInt = BigInt { negative:false, data: vec![m as u64]};
@@ -57,14 +102,22 @@ println!("{} x {}\n",a,b);
         data: b.data.get(m2..b.data.len()).unwrap().to_vec()
     };
 
+    print_bigint(&low1);
+    print_bigint(&high1);
+    print_bigint(&low2);
+    print_bigint(&high2);
+    println!("");
     /*
     high1, low1 = split_at(num1, m2)
     high2, low2 = split_at(num2, m2)
     */
 
     /* 3 calls made to numbers approximately half the size. */
+    println!("Getting z0");
     let z0:BigInt = karatsuba_mul(&low1, &low2);
+    println!("Getting z1");
     let z1:BigInt = karatsuba_mul(&(low1 + &high1), &(&low2 + &high2));
+    println!("Getting z2");
     let z2:BigInt = karatsuba_mul(&high1, &high2);
 
     let mut first:BigInt = &two << (&m2_bi * &two);
