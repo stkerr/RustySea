ed2dbade3698be85fd5fd2b1b40a32284535e9c7
Sam Kerr <skerr@gitlab.com>
2022-05-20 16:22:06 -0400

    Add additional 900 tests and bit shift tests

--- src/testing/dynamic_tests/generator.py
+++ src/testing/dynamic_tests/generator.py
@@ -1,15 +1,15 @@
 import random
 
-ops = [ # op, function, exponent range, name
-    ['+', lambda x,y: x+y, 256, 'add'],
-    ['-', lambda x,y: x-y, 256, 'sub'],
-    ['*', lambda x,y: x*y, 64, 'mul'],
+ops = [ # op, function, exponent range, name, allow_negative_x, allow_negative_y
+    ['+', lambda x,y: x+y, 256, 'add', True, True],
+    ['-', lambda x,y: x-y, 256, 'sub', True, True],
+    ['*', lambda x,y: x*y, 64, 'mul', True, True],
     #['/', lambda x,y: x/y],
-    #['<<', lambda x,y: x<<y],
-    #['>>', lambda x,y: x>>y],
-    ['^', lambda x,y: x^y, 256, 'bitxor'],
-    ['|', lambda x,y: x|y, 256, 'bitor'],
-    ['&', lambda x,y: x&y, 256, 'bitand']
+    ['<<', lambda x,y: x<<abs(y), 16, 'bitshl', False, False],
+    ['>>', lambda x,y: x>>abs(y), 16, 'bitshr', False, False],
+    ['^', lambda x,y: x^y, 256, 'bitxor', True, True],
+    ['|', lambda x,y: x|y, 256, 'bitor', True, True],
+    ['&', lambda x,y: x&y, 256, 'bitand', True, True]
 ]
 
 header = """
@@ -25,12 +25,17 @@ op_test!(%s, "%s" %s "%s" == "%s");
 with open('dynamic_tests.rs', 'w') as f:
     f.write(header)
 
-    tests = 100
+    tests = 1000
     for i in range(tests):
         op = random.choice(ops)
 
         x = random.randint(-2<<op[2], 2<<op[2])
+        if not op[4]:
+            x = abs(x)
+
         y = random.randint(-2<<op[2], 2<<op[2])
+        if not op[5]:
+            y = abs(y)
 
         test = template % (
                 "dynamic_test_%s_%s" % (op[3], i),
